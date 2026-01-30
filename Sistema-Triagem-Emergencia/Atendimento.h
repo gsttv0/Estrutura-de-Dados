ee93867c26f411bd57cd36998cbb84b0f042e11f
Gustavo Schmitt dos Santos <123377688+gsttv0@users.noreply.github.com>
2026-01-30 10:36:25 -0300

    Create Atendimento.h

---
+++ Sistema-Triagem-Emergencia/Atendimento.h
@@ -0,0 +1,15 @@
+#ifndef ATENDIMENTO_H
+#define ATENDIMENTO_H
+#include "FilaDuplamenteEncadeada.h"
+#include <iostream>
+
+using namespace std;
+
+struct Paciente{
+    string nome;
+    string condicao;
+    int prio;
+};
+
+// ... O RESTO DO SEU CÓDIGO PERMANECE O MESMO ...
+// Só uma dica: No 'default' do switch, você pode adicionar uma quebra de linha.
